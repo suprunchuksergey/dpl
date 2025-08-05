0743ac7e950f3f146ad42d7b03796325034afc02
suprunchuksergey <suprunchuksergey@proton.me>
2025-08-05 20:06:37 +0700

    добавил парсер

--- parser/parser.go
+++ parser/parser.go
@@ -1,1 +1,345 @@
 package parser
+
+import (
+	"fmt"
+	"github.com/suprunchuksergey/dpl/lexer"
+	"github.com/suprunchuksergey/dpl/node"
+	"github.com/suprunchuksergey/dpl/token"
+	"strconv"
+)
+
+/*
+parser реализует рекурсивный спуск для разбора выражений
+с учетом приоритета операций.
+
+слои (layer[p], где p — приоритет, меньший p = выше приоритет)
+обрабатывают операторы (например, layer4: *, /, %; layer5: +, -) и операнды.
+
+слои:
+layer1 -> true, false, null, int, real, text
+layer2 -> ()
+layer3 -> - (унарный)
+layer4 -> *, /, %
+layer5 -> +, -
+layer6 -> ||
+layer7 -> =, !=, <, <=, >, >=
+layer8 -> not
+layer9 -> and
+layer10 -> or
+*/
+type parser struct{ lex lexer.Lexer }
+
+// true, false, null, int, real, text
+func (p *parser) layer1() (node.Node, error) {
+	var n node.Node
+
+	switch tok := p.lex.Tok(); tok.ID() {
+	case token.Null:
+		n = node.Null()
+	case token.True:
+		n = node.True()
+	case token.False:
+		n = node.False()
+	case token.Int:
+		v := tok.(token.WithValue).Value()
+		num, err := strconv.ParseInt(v, 10, 64)
+		if err != nil {
+			return nil, err
+		}
+		n = node.Int(num)
+	case token.Real:
+		v := tok.(token.WithValue).Value()
+		num, err := strconv.ParseFloat(v, 64)
+		if err != nil {
+			return nil, err
+		}
+		n = node.Real(num)
+	case token.Text:
+		n = node.Text(
+			tok.(token.WithValue).Value())
+	default:
+		return nil, unexpected(tok)
+	}
+
+	err := p.lex.Next()
+	if err != nil {
+		return nil, err
+	}
+	return n, nil
+}
+
+// ()
+func (p *parser) layer2() (node.Node, error) {
+	if p.lex.Tok().ID() != token.LParen {
+		return p.layer1()
+	}
+
+	err := p.lex.Next()
+	if err != nil {
+		return nil, err
+	}
+
+	n, err := p.layer10()
+	if err != nil {
+		return nil, err
+	}
+
+	if p.lex.Tok().ID() != token.RParen {
+		return nil, unexpected(p.lex.Tok())
+	}
+
+	err = p.lex.Next()
+	if err != nil {
+		return nil, err
+	}
+
+	return n, nil
+}
+
+// - (унарный)
+func (p *parser) layer3() (node.Node, error) {
+	if p.lex.Tok().ID() != token.Sub {
+		return p.layer2()
+	}
+
+	err := p.lex.Next()
+	if err != nil {
+		return nil, err
+	}
+
+	n, err := p.layer3()
+	if err != nil {
+		return nil, err
+	}
+
+	return node.Neg(n), nil
+}
+
+// *, /, %
+func (p *parser) layer4() (node.Node, error) {
+	n, err := p.layer3()
+	if err != nil {
+		return nil, err
+	}
+
+	for p.lex.Tok().OneOf(token.Mul, token.Div, token.Rem) {
+		id := p.lex.Tok().ID()
+
+		err := p.lex.Next()
+		if err != nil {
+			return nil, err
+		}
+
+		r, err := p.layer3()
+		if err != nil {
+			return nil, err
+		}
+
+		switch id {
+		case token.Mul:
+			n = node.Mul(n, r)
+		case token.Div:
+			n = node.Div(n, r)
+		default:
+			n = node.Rem(n, r)
+		}
+	}
+
+	return n, nil
+}
+
+// +, -
+func (p *parser) layer5() (node.Node, error) {
+	n, err := p.layer4()
+	if err != nil {
+		return nil, err
+	}
+
+	for p.lex.Tok().OneOf(token.Add, token.Sub) {
+		id := p.lex.Tok().ID()
+
+		err := p.lex.Next()
+		if err != nil {
+			return nil, err
+		}
+
+		r, err := p.layer4()
+		if err != nil {
+			return nil, err
+		}
+
+		switch id {
+		case token.Add:
+			n = node.Add(n, r)
+		default:
+			n = node.Sub(n, r)
+		}
+	}
+
+	return n, nil
+}
+
+// ||
+func (p *parser) layer6() (node.Node, error) {
+	n, err := p.layer5()
+	if err != nil {
+		return nil, err
+	}
+
+	for p.lex.Tok().Is(token.Concat) {
+		err := p.lex.Next()
+		if err != nil {
+			return nil, err
+		}
+
+		r, err := p.layer5()
+		if err != nil {
+			return nil, err
+		}
+
+		n = node.Concat(n, r)
+	}
+
+	return n, nil
+}
+
+// =, !=, <, <=, >, >=
+func (p *parser) layer7() (node.Node, error) {
+	n, err := p.layer6()
+	if err != nil {
+		return nil, err
+	}
+
+	for p.lex.Tok().OneOf(token.Eq, token.Neq, token.Lt,
+		token.Gt, token.Lte, token.Gte) {
+		id := p.lex.Tok().ID()
+
+		err := p.lex.Next()
+		if err != nil {
+			return nil, err
+		}
+
+		r, err := p.layer6()
+		if err != nil {
+			return nil, err
+		}
+
+		switch id {
+		case token.Eq:
+			n = node.Eq(n, r)
+		case token.Neq:
+			n = node.Neq(n, r)
+		case token.Lt:
+			n = node.Lt(n, r)
+		case token.Gt:
+			n = node.Gt(n, r)
+		case token.Lte:
+			n = node.Lte(n, r)
+		default:
+			n = node.Gte(n, r)
+		}
+	}
+
+	return n, nil
+}
+
+// not
+func (p *parser) layer8() (node.Node, error) {
+	if p.lex.Tok().ID() != token.Not {
+		return p.layer7()
+	}
+
+	err := p.lex.Next()
+	if err != nil {
+		return nil, err
+	}
+
+	n, err := p.layer8()
+	if err != nil {
+		return nil, err
+	}
+
+	return node.Not(n), nil
+}
+
+// and
+func (p *parser) layer9() (node.Node, error) {
+	n, err := p.layer8()
+	if err != nil {
+		return nil, err
+	}
+
+	for p.lex.Tok().Is(token.And) {
+		err := p.lex.Next()
+		if err != nil {
+			return nil, err
+		}
+
+		r, err := p.layer8()
+		if err != nil {
+			return nil, err
+		}
+
+		n = node.And(n, r)
+	}
+
+	return n, nil
+}
+
+// or
+func (p *parser) layer10() (node.Node, error) {
+	n, err := p.layer9()
+	if err != nil {
+		return nil, err
+	}
+
+	for p.lex.Tok().Is(token.Or) {
+		err := p.lex.Next()
+		if err != nil {
+			return nil, err
+		}
+
+		r, err := p.layer9()
+		if err != nil {
+			return nil, err
+		}
+
+		n = node.Or(n, r)
+	}
+
+	return n, nil
+}
+
+func (p *parser) Parse() (node.Node, error) {
+	//если это конец
+	if p.lex.Tok().Is(token.EOF) {
+		return nil, nil
+	}
+
+	n, err := p.layer10()
+	if err != nil {
+		return nil, err
+	}
+
+	//если это не конец
+	if p.lex.Tok().ID() != token.EOF {
+		return nil, unexpected(p.lex.Tok())
+	}
+
+	return n, nil
+}
+
+func newParser(lex lexer.Lexer) *parser { return &parser{lex} }
+
+type Parser interface {
+	//возвращает узел или ошибку,
+	//узел будет nil, если первый токен EOF
+	Parse() (node.Node, error)
+}
+
+// у лексера должен быть прочитан первый токен
+func New(lex lexer.Lexer) Parser { return newParser(lex) }
+
+func unexpected(tok token.Token) error {
+	return fmt.Errorf("%s неожиданный токен %s", tok.Start(), tok)
+}
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -1,1 +1,738 @@
 package parser
+
+import (
+	"fmt"
+	"github.com/suprunchuksergey/dpl/lexer"
+	"github.com/suprunchuksergey/dpl/node"
+	"github.com/suprunchuksergey/dpl/pos"
+	"github.com/suprunchuksergey/dpl/token"
+	"testing"
+)
+
+const (
+	_ uint8 = iota
+	parse
+	layer1
+	layer2
+	layer3
+	layer4
+	layer5
+	layer6
+	layer7
+	layer8
+	layer9
+	layer10
+)
+
+func call(p *parser, l uint8) (node.Node, error) {
+	switch l {
+	case parse:
+		return p.Parse()
+	case layer1:
+		return p.layer1()
+	case layer2:
+		return p.layer2()
+	case layer3:
+		return p.layer3()
+	case layer4:
+		return p.layer4()
+	case layer5:
+		return p.layer5()
+	case layer6:
+		return p.layer6()
+	case layer7:
+		return p.layer7()
+	case layer8:
+		return p.layer8()
+	case layer9:
+		return p.layer9()
+	case layer10:
+		return p.layer10()
+	default:
+		panic("неизвестный слой")
+	}
+}
+
+func prepare(data string) (*parser, error) {
+	lex := lexer.New(data)
+	err := lex.Next()
+	if err != nil {
+		return nil, fmt.Errorf("%s: %s", data, err.Error())
+	}
+	return newParser(lex), nil
+}
+
+type row struct {
+	data     string
+	expected node.Node
+}
+
+func (r row) exec(t *testing.T, l uint8) {
+	p, err := prepare(r.data)
+	if err != nil {
+		t.Error(err.Error())
+		return
+	}
+
+	v, err := call(p, l)
+	if err != nil {
+		t.Errorf("%s: %s", r.data, err.Error())
+		return
+	}
+
+	if node.DeepEqual(r.expected, v) == false {
+		t.Errorf("%s: ожидалось %s, получено %s",
+			r.data, r.expected, v)
+	}
+}
+
+func r(data string, expected node.Node) row {
+	return row{
+		data:     data,
+		expected: expected,
+	}
+}
+
+type rows []row
+
+func (rows rows) exec(t *testing.T, l uint8) {
+	for _, row := range rows {
+		row.exec(t, l)
+	}
+}
+
+func rs(rs ...row) rows {
+	return rs
+}
+
+func Test_layer1(t *testing.T) {
+	rs(
+		r("true", node.True()),
+		r("false", node.False()),
+		r("null", node.Null()),
+		r("19683", node.Int(19683)),
+		r("19.683", node.Real(19.683)),
+		r("'text'", node.Text("text")),
+	).exec(t, layer1)
+}
+
+func Test_layer3(t *testing.T) {
+	rs(
+		r("true", node.True()),
+		r("false", node.False()),
+		r("null", node.Null()),
+		r("19683", node.Int(19683)),
+		r("19.683", node.Real(19.683)),
+		r("'text'", node.Text("text")),
+		r("-19", node.Neg(node.Int(19))),
+		r("--19",
+			node.Neg(
+				node.Neg(
+					node.Int(19)))),
+		r("---19",
+			node.Neg(
+				node.Neg(
+					node.Neg(
+						node.Int(19))))),
+	).exec(t, layer3)
+}
+
+func Test_layer4(t *testing.T) {
+	rs(
+		r("true", node.True()),
+		r("false", node.False()),
+		r("null", node.Null()),
+		r("19683", node.Int(19683)),
+		r("19.683", node.Real(19.683)),
+		r("'text'", node.Text("text")),
+		r("-19", node.Neg(node.Int(19))),
+		r("--19",
+			node.Neg(
+				node.Neg(
+					node.Int(19)))),
+		r("---19",
+			node.Neg(
+				node.Neg(
+					node.Neg(
+						node.Int(19))))),
+		r("19*683", node.Mul(node.Int(19), node.Int(683))),
+		r("19/683", node.Div(node.Int(19), node.Int(683))),
+		r("19%683", node.Rem(node.Int(19), node.Int(683))),
+		r("19*683/-3",
+			node.Div(
+				node.Mul(node.Int(19), node.Int(683)),
+				node.Neg(node.Int(3)))),
+	).exec(t, layer4)
+}
+
+func Test_layer5(t *testing.T) {
+	rs(
+		r("true", node.True()),
+		r("false", node.False()),
+		r("null", node.Null()),
+		r("19683", node.Int(19683)),
+		r("19.683", node.Real(19.683)),
+		r("'text'", node.Text("text")),
+		r("-19", node.Neg(node.Int(19))),
+		r("--19",
+			node.Neg(
+				node.Neg(
+					node.Int(19)))),
+		r("---19",
+			node.Neg(
+				node.Neg(
+					node.Neg(
+						node.Int(19))))),
+		r("19*683", node.Mul(node.Int(19), node.Int(683))),
+		r("19/683", node.Div(node.Int(19), node.Int(683))),
+		r("19%683", node.Rem(node.Int(19), node.Int(683))),
+		r("19*683/-3",
+			node.Div(
+				node.Mul(node.Int(19), node.Int(683)),
+				node.Neg(node.Int(3)))),
+		r("19+683", node.Add(node.Int(19), node.Int(683))),
+		r("19-683", node.Sub(node.Int(19), node.Int(683))),
+		r("19--683", node.Sub(node.Int(19), node.Neg(node.Int(683)))),
+		r("19*683/-3+83",
+			node.Add(
+				node.Div(
+					node.Mul(node.Int(19), node.Int(683)),
+					node.Neg(node.Int(3))),
+				node.Int(83))),
+	).exec(t, layer5)
+}
+
+func Test_layer6(t *testing.T) {
+	rs(
+		r("true", node.True()),
+		r("false", node.False()),
+		r("null", node.Null()),
+		r("19683", node.Int(19683)),
+		r("19.683", node.Real(19.683)),
+		r("'text'", node.Text("text")),
+		r("-19", node.Neg(node.Int(19))),
+		r("--19",
+			node.Neg(
+				node.Neg(
+					node.Int(19)))),
+		r("---19",
+			node.Neg(
+				node.Neg(
+					node.Neg(
+						node.Int(19))))),
+		r("19*683", node.Mul(node.Int(19), node.Int(683))),
+		r("19/683", node.Div(node.Int(19), node.Int(683))),
+		r("19%683", node.Rem(node.Int(19), node.Int(683))),
+		r("19*683/-3",
+			node.Div(
+				node.Mul(node.Int(19), node.Int(683)),
+				node.Neg(node.Int(3)))),
+		r("19+683", node.Add(node.Int(19), node.Int(683))),
+		r("19-683", node.Sub(node.Int(19), node.Int(683))),
+		r("19--683", node.Sub(node.Int(19), node.Neg(node.Int(683)))),
+		r("19*683/-3+83",
+			node.Add(
+				node.Div(
+					node.Mul(node.Int(19), node.Int(683)),
+					node.Neg(node.Int(3))),
+				node.Int(83))),
+		r("'hello'||'world'",
+			node.Concat(
+				node.Text("hello"),
+				node.Text("world"))),
+		r("19*683/-3+83||'рублей'",
+			node.Concat(
+				node.Add(
+					node.Div(
+						node.Mul(node.Int(19), node.Int(683)),
+						node.Neg(node.Int(3))),
+					node.Int(83)),
+				node.Text("рублей"),
+			)),
+		r("'рублей'||19*683/-3+83",
+			node.Concat(
+				node.Text("рублей"),
+				node.Add(
+					node.Div(
+						node.Mul(node.Int(19), node.Int(683)),
+						node.Neg(node.Int(3))),
+					node.Int(83)),
+			)),
+	).exec(t, layer6)
+}
+
+func Test_layer7(t *testing.T) {
+	rs(
+		r("true", node.True()),
+		r("false", node.False()),
+		r("null", node.Null()),
+		r("19683", node.Int(19683)),
+		r("19.683", node.Real(19.683)),
+		r("'text'", node.Text("text")),
+		r("-19", node.Neg(node.Int(19))),
+		r("--19",
+			node.Neg(
+				node.Neg(
+					node.Int(19)))),
+		r("---19",
+			node.Neg(
+				node.Neg(
+					node.Neg(
+						node.Int(19))))),
+		r("19*683", node.Mul(node.Int(19), node.Int(683))),
+		r("19/683", node.Div(node.Int(19), node.Int(683))),
+		r("19%683", node.Rem(node.Int(19), node.Int(683))),
+		r("19*683/-3",
+			node.Div(
+				node.Mul(node.Int(19), node.Int(683)),
+				node.Neg(node.Int(3)))),
+		r("19+683", node.Add(node.Int(19), node.Int(683))),
+		r("19-683", node.Sub(node.Int(19), node.Int(683))),
+		r("19--683", node.Sub(node.Int(19), node.Neg(node.Int(683)))),
+		r("19*683/-3+83",
+			node.Add(
+				node.Div(
+					node.Mul(node.Int(19), node.Int(683)),
+					node.Neg(node.Int(3))),
+				node.Int(83))),
+		r("'hello'||'world'",
+			node.Concat(
+				node.Text("hello"),
+				node.Text("world"))),
+		r("19*683/-3+83||'рублей'",
+			node.Concat(
+				node.Add(
+					node.Div(
+						node.Mul(node.Int(19), node.Int(683)),
+						node.Neg(node.Int(3))),
+					node.Int(83)),
+				node.Text("рублей"),
+			)),
+		r("'рублей'||19*683/-3+83",
+			node.Concat(
+				node.Text("рублей"),
+				node.Add(
+					node.Div(
+						node.Mul(node.Int(19), node.Int(683)),
+						node.Neg(node.Int(3))),
+					node.Int(83)),
+			)),
+		r("19=683", node.Eq(node.Int(19), node.Int(683))),
+		r("19!=683", node.Neq(node.Int(19), node.Int(683))),
+		r("19<683", node.Lt(node.Int(19), node.Int(683))),
+		r("19<=683", node.Lte(node.Int(19), node.Int(683))),
+		r("19>683", node.Gt(node.Int(19), node.Int(683))),
+		r("19>=683", node.Gte(node.Int(19), node.Int(683))),
+		r("19+83=68*-3",
+			node.Eq(
+				node.Add(node.Int(19), node.Int(83)),
+				node.Mul(node.Int(68), node.Neg(node.Int(3))))),
+		r("19=683!=true",
+			node.Neq(
+				node.Eq(node.Int(19), node.Int(683)),
+				node.True())),
+	).exec(t, layer7)
+}
+
+func Test_layer8(t *testing.T) {
+	rs(
+		r("true", node.True()),
+		r("false", node.False()),
+		r("null", node.Null()),
+		r("19683", node.Int(19683)),
+		r("19.683", node.Real(19.683)),
+		r("'text'", node.Text("text")),
+		r("-19", node.Neg(node.Int(19))),
+		r("--19",
+			node.Neg(
+				node.Neg(
+					node.Int(19)))),
+		r("---19",
+			node.Neg(
+				node.Neg(
+					node.Neg(
+						node.Int(19))))),
+		r("19*683", node.Mul(node.Int(19), node.Int(683))),
+		r("19/683", node.Div(node.Int(19), node.Int(683))),
+		r("19%683", node.Rem(node.Int(19), node.Int(683))),
+		r("19*683/-3",
+			node.Div(
+				node.Mul(node.Int(19), node.Int(683)),
+				node.Neg(node.Int(3)))),
+		r("19+683", node.Add(node.Int(19), node.Int(683))),
+		r("19-683", node.Sub(node.Int(19), node.Int(683))),
+		r("19--683", node.Sub(node.Int(19), node.Neg(node.Int(683)))),
+		r("19*683/-3+83",
+			node.Add(
+				node.Div(
+					node.Mul(node.Int(19), node.Int(683)),
+					node.Neg(node.Int(3))),
+				node.Int(83))),
+		r("'hello'||'world'",
+			node.Concat(
+				node.Text("hello"),
+				node.Text("world"))),
+		r("19*683/-3+83||'рублей'",
+			node.Concat(
+				node.Add(
+					node.Div(
+						node.Mul(node.Int(19), node.Int(683)),
+						node.Neg(node.Int(3))),
+					node.Int(83)),
+				node.Text("рублей"),
+			)),
+		r("'рублей'||19*683/-3+83",
+			node.Concat(
+				node.Text("рублей"),
+				node.Add(
+					node.Div(
+						node.Mul(node.Int(19), node.Int(683)),
+						node.Neg(node.Int(3))),
+					node.Int(83)),
+			)),
+		r("19=683", node.Eq(node.Int(19), node.Int(683))),
+		r("19!=683", node.Neq(node.Int(19), node.Int(683))),
+		r("19<683", node.Lt(node.Int(19), node.Int(683))),
+		r("19<=683", node.Lte(node.Int(19), node.Int(683))),
+		r("19>683", node.Gt(node.Int(19), node.Int(683))),
+		r("19>=683", node.Gte(node.Int(19), node.Int(683))),
+		r("19+83=68*-3",
+			node.Eq(
+				node.Add(node.Int(19), node.Int(83)),
+				node.Mul(node.Int(68), node.Neg(node.Int(3))))),
+		r("19=683!=true",
+			node.Neq(
+				node.Eq(node.Int(19), node.Int(683)),
+				node.True())),
+		r("not true", node.Not(node.True())),
+		r("not not true",
+			node.Not(
+				node.Not(
+					node.True()))),
+		r("not not not true",
+			node.Not(
+				node.Not(
+					node.Not(
+						node.True())))),
+		r("not 19+83=68*-3",
+			node.Not(
+				node.Eq(
+					node.Add(node.Int(19), node.Int(83)),
+					node.Mul(node.Int(68), node.Neg(node.Int(3)))))),
+	).exec(t, layer8)
+}
+
+func Test_layer9(t *testing.T) {
+	rs(
+		r("true", node.True()),
+		r("false", node.False()),
+		r("null", node.Null()),
+		r("19683", node.Int(19683)),
+		r("19.683", node.Real(19.683)),
+		r("'text'", node.Text("text")),
+		r("-19", node.Neg(node.Int(19))),
+		r("--19",
+			node.Neg(
+				node.Neg(
+					node.Int(19)))),
+		r("---19",
+			node.Neg(
+				node.Neg(
+					node.Neg(
+						node.Int(19))))),
+		r("19*683", node.Mul(node.Int(19), node.Int(683))),
+		r("19/683", node.Div(node.Int(19), node.Int(683))),
+		r("19%683", node.Rem(node.Int(19), node.Int(683))),
+		r("19*683/-3",
+			node.Div(
+				node.Mul(node.Int(19), node.Int(683)),
+				node.Neg(node.Int(3)))),
+		r("19+683", node.Add(node.Int(19), node.Int(683))),
+		r("19-683", node.Sub(node.Int(19), node.Int(683))),
+		r("19--683", node.Sub(node.Int(19), node.Neg(node.Int(683)))),
+		r("19*683/-3+83",
+			node.Add(
+				node.Div(
+					node.Mul(node.Int(19), node.Int(683)),
+					node.Neg(node.Int(3))),
+				node.Int(83))),
+		r("'hello'||'world'",
+			node.Concat(
+				node.Text("hello"),
+				node.Text("world"))),
+		r("19*683/-3+83||'рублей'",
+			node.Concat(
+				node.Add(
+					node.Div(
+						node.Mul(node.Int(19), node.Int(683)),
+						node.Neg(node.Int(3))),
+					node.Int(83)),
+				node.Text("рублей"),
+			)),
+		r("'рублей'||19*683/-3+83",
+			node.Concat(
+				node.Text("рублей"),
+				node.Add(
+					node.Div(
+						node.Mul(node.Int(19), node.Int(683)),
+						node.Neg(node.Int(3))),
+					node.Int(83)),
+			)),
+		r("19=683", node.Eq(node.Int(19), node.Int(683))),
+		r("19!=683", node.Neq(node.Int(19), node.Int(683))),
+		r("19<683", node.Lt(node.Int(19), node.Int(683))),
+		r("19<=683", node.Lte(node.Int(19), node.Int(683))),
+		r("19>683", node.Gt(node.Int(19), node.Int(683))),
+		r("19>=683", node.Gte(node.Int(19), node.Int(683))),
+		r("19+83=68*-3",
+			node.Eq(
+				node.Add(node.Int(19), node.Int(83)),
+				node.Mul(node.Int(68), node.Neg(node.Int(3))))),
+		r("19=683!=true",
+			node.Neq(
+				node.Eq(node.Int(19), node.Int(683)),
+				node.True())),
+		r("not true", node.Not(node.True())),
+		r("not not true",
+			node.Not(
+				node.Not(
+					node.True()))),
+		r("not not not true",
+			node.Not(
+				node.Not(
+					node.Not(
+						node.True())))),
+		r("not 19+83=68*-3",
+			node.Not(
+				node.Eq(
+					node.Add(node.Int(19), node.Int(83)),
+					node.Mul(node.Int(68), node.Neg(node.Int(3)))))),
+		r("true and false", node.And(node.True(), node.False())),
+		r("not 19+83=68*-3 and true",
+			node.And(
+				node.Not(
+					node.Eq(
+						node.Add(node.Int(19), node.Int(83)),
+						node.Mul(node.Int(68), node.Neg(node.Int(3))))),
+				node.True())),
+	).exec(t, layer9)
+}
+
+func Test_layer10(t *testing.T) {
+	rs(
+		r("true", node.True()),
+		r("false", node.False()),
+		r("null", node.Null()),
+		r("19683", node.Int(19683)),
+		r("19.683", node.Real(19.683)),
+		r("'text'", node.Text("text")),
+		r("-19", node.Neg(node.Int(19))),
+		r("--19",
+			node.Neg(
+				node.Neg(
+					node.Int(19)))),
+		r("---19",
+			node.Neg(
+				node.Neg(
+					node.Neg(
+						node.Int(19))))),
+		r("19*683", node.Mul(node.Int(19), node.Int(683))),
+		r("19/683", node.Div(node.Int(19), node.Int(683))),
+		r("19%683", node.Rem(node.Int(19), node.Int(683))),
+		r("19*683/-3",
+			node.Div(
+				node.Mul(node.Int(19), node.Int(683)),
+				node.Neg(node.Int(3)))),
+		r("19+683", node.Add(node.Int(19), node.Int(683))),
+		r("19-683", node.Sub(node.Int(19), node.Int(683))),
+		r("19--683", node.Sub(node.Int(19), node.Neg(node.Int(683)))),
+		r("19*683/-3+83",
+			node.Add(
+				node.Div(
+					node.Mul(node.Int(19), node.Int(683)),
+					node.Neg(node.Int(3))),
+				node.Int(83))),
+		r("'hello'||'world'",
+			node.Concat(
+				node.Text("hello"),
+				node.Text("world"))),
+		r("19*683/-3+83||'рублей'",
+			node.Concat(
+				node.Add(
+					node.Div(
+						node.Mul(node.Int(19), node.Int(683)),
+						node.Neg(node.Int(3))),
+					node.Int(83)),
+				node.Text("рублей"),
+			)),
+		r("'рублей'||19*683/-3+83",
+			node.Concat(
+				node.Text("рублей"),
+				node.Add(
+					node.Div(
+						node.Mul(node.Int(19), node.Int(683)),
+						node.Neg(node.Int(3))),
+					node.Int(83)),
+			)),
+		r("19=683", node.Eq(node.Int(19), node.Int(683))),
+		r("19!=683", node.Neq(node.Int(19), node.Int(683))),
+		r("19<683", node.Lt(node.Int(19), node.Int(683))),
+		r("19<=683", node.Lte(node.Int(19), node.Int(683))),
+		r("19>683", node.Gt(node.Int(19), node.Int(683))),
+		r("19>=683", node.Gte(node.Int(19), node.Int(683))),
+		r("19+83=68*-3",
+			node.Eq(
+				node.Add(node.Int(19), node.Int(83)),
+				node.Mul(node.Int(68), node.Neg(node.Int(3))))),
+		r("19=683!=true",
+			node.Neq(
+				node.Eq(node.Int(19), node.Int(683)),
+				node.True())),
+		r("not true", node.Not(node.True())),
+		r("not not true",
+			node.Not(
+				node.Not(
+					node.True()))),
+		r("not not not true",
+			node.Not(
+				node.Not(
+					node.Not(
+						node.True())))),
+		r("not 19+83=68*-3",
+			node.Not(
+				node.Eq(
+					node.Add(node.Int(19), node.Int(83)),
+					node.Mul(node.Int(68), node.Neg(node.Int(3)))))),
+		r("true and false", node.And(node.True(), node.False())),
+		r("not 19+83=68*-3 and true",
+			node.And(
+				node.Not(
+					node.Eq(
+						node.Add(node.Int(19), node.Int(83)),
+						node.Mul(node.Int(68), node.Neg(node.Int(3))))),
+				node.True())),
+		r("true or false", node.Or(node.True(), node.False())),
+		r("not 19+83=68*-3 or true",
+			node.Or(
+				node.Not(
+					node.Eq(
+						node.Add(node.Int(19), node.Int(83)),
+						node.Mul(node.Int(68), node.Neg(node.Int(3))))),
+				node.True())),
+		r("true and false or true and true",
+			node.Or(
+				node.And(node.True(), node.False()),
+				node.And(node.True(), node.True()),
+			)),
+	).exec(t, layer10)
+}
+
+func Test_layer2(t *testing.T) {
+	rs(
+		r("true", node.True()),
+		r("false", node.False()),
+		r("null", node.Null()),
+		r("19683", node.Int(19683)),
+		r("19.683", node.Real(19.683)),
+		r("'text'", node.Text("text")),
+		r("(19*683)", node.Mul(node.Int(19), node.Int(683))),
+		r("(19/683)", node.Div(node.Int(19), node.Int(683))),
+		r("(19%683)", node.Rem(node.Int(19), node.Int(683))),
+	).exec(t, layer2)
+}
+
+func Test_Parse(t *testing.T) {
+	rs(
+		r("81+16*16",
+			node.Add(
+				node.Int(81),
+				node.Mul(node.Int(16), node.Int(16)),
+			),
+		),
+		r("(81+16)*16",
+			node.Mul(
+				node.Add(node.Int(81), node.Int(16)),
+				node.Int(16),
+			),
+		),
+		r("(81+16)*-(64/16)",
+			node.Mul(
+				node.Add(node.Int(81), node.Int(16)),
+				node.Neg(node.Div(node.Int(64), node.Int(16))),
+			),
+		),
+		r("(81+16)*-(64/16)<16%6-1",
+			node.Lt(
+				node.Mul(
+					node.Add(node.Int(81), node.Int(16)),
+					node.Neg(node.Div(node.Int(64), node.Int(16))),
+				),
+				node.Sub(
+					node.Rem(node.Int(16), node.Int(6)),
+					node.Int(1),
+				),
+			),
+		),
+		r("(81+16)*-(64/16)<16%6-1 and not true",
+			node.And(
+				node.Lt(
+					node.Mul(
+						node.Add(node.Int(81), node.Int(16)),
+						node.Neg(node.Div(node.Int(64), node.Int(16))),
+					),
+					node.Sub(
+						node.Rem(node.Int(16), node.Int(6)),
+						node.Int(1),
+					),
+				),
+				node.Not(node.True()),
+			),
+		),
+		r("(81+16)*-(64/16)<16%6-1 and not true or not not true",
+			node.Or(
+				node.And(
+					node.Lt(
+						node.Mul(
+							node.Add(node.Int(81), node.Int(16)),
+							node.Neg(node.Div(node.Int(64), node.Int(16))),
+						),
+						node.Sub(
+							node.Rem(node.Int(16), node.Int(6)),
+							node.Int(1),
+						),
+					),
+					node.Not(node.True()),
+				),
+				node.Not(node.Not(node.True())),
+			),
+		),
+	).exec(t, parse)
+}
+
+func Test_Parse_errs(t *testing.T) {
+	//все тесты должны пройти с ошибками
+	tests := []struct {
+		data     string
+		expected error
+	}{
+		{"(81+16", unexpected(token.New(token.EOF, pos.NewWithStart(1, 7)))},
+		{"81+16*", unexpected(token.New(token.EOF, pos.NewWithStart(1, 7)))},
+		{"-", unexpected(token.New(token.EOF, pos.NewWithStart(1, 2)))},
+		{"16 true", unexpected(token.New(token.True, pos.NewWithStart(1, 4)))},
+		{")", unexpected(token.New(token.RParen, pos.NewWithStart(1, 1)))},
+	}
+
+	for i, test := range tests {
+		p, err := prepare(test.data)
+		if err != nil {
+			t.Error(err.Error())
+			continue
+		}
+
+		_, err = p.Parse()
+		if err.Error() != test.expected.Error() {
+			t.Errorf("%d: ожидалось %q, получено %q",
+				i, test.expected.Error(), err.Error())
+		}
+	}
+}
