4039e7f598dfa00332f5e6b1ba8f41849afa9d72
suprunchuksergey <suprunchuksergey@proton.me>
2025-09-17 17:55:24 +0700

    добавил встроенные функции len, append, print и println

--- dpl.go
+++ dpl.go
@@ -1,6 +1,8 @@
 package dpl
 
 import (
+	"errors"
+	"fmt"
 	"github.com/suprunchuksergey/dpl/internal/lexer"
 	"github.com/suprunchuksergey/dpl/internal/namespace"
 	"github.com/suprunchuksergey/dpl/internal/parser"
@@ -18,5 +20,62 @@ func Exec(program string, init map[string]value.Value) (value.Value, error) {
 		return nil, err
 	}
 
-	return n.Exec(namespace.New(init))
+	return n.Exec(initNamespace(init))
+}
+
+func builtinLen(args ...value.Value) (value.Value, error) {
+	if len(args) == 0 {
+		return nil, errors.New("len: требуется один аргумент")
+	}
+	v := args[0]
+	l, err := v.Len()
+	if err != nil {
+		return nil, err
+	}
+	return value.Int(l), nil
+}
+
+func builtinAppend(args ...value.Value) (value.Value, error) {
+	if len(args) == 0 {
+		return nil, errors.New("append: требуется один аргумент")
+	}
+	v := args[0]
+	return v.Append(args[1:]...)
+}
+
+func builtinPrint(args ...value.Value) (value.Value, error) {
+	a := make([]any, 0, len(args))
+	for _, v := range args {
+		a = append(a, v.String())
+	}
+
+	fmt.Print(a...)
+
+	return value.Null(), nil
+}
+
+func builtinPrintln(args ...value.Value) (value.Value, error) {
+	a := make([]any, 0, len(args))
+	for _, v := range args {
+		a = append(a, v.String())
+	}
+
+	fmt.Println(a...)
+
+	return value.Null(), nil
+}
+
+func initNamespace(init map[string]value.Value) namespace.Namespace {
+	m := map[string]value.Value{
+		"len":     value.Function(builtinLen),
+		"append":  value.Function(builtinAppend),
+		"print":   value.Function(builtinPrint),
+		"println": value.Function(builtinPrintln),
+	}
+
+	for k, v := range init {
+		m[k] = v
+	}
+
+	return namespace.New(m)
 }
--- dpl_test.go
+++ dpl_test.go
@@ -1,6 +1,7 @@
 package dpl
 
 import (
+	"errors"
 	"github.com/stretchr/testify/assert"
 	"github.com/suprunchuksergey/dpl/internal/value"
 	"testing"
@@ -28,10 +29,98 @@ for i in 8 {
 
 sum;
 `, value.Int(5914), nil},
+
+		{`
+arr := [];
+
+for i in 4 {
+	arr = append(arr,i);
+};
+
+arr = append(arr,arr[len(arr)-1]);
+
+arr;
+`, value.Array(value.Int(0), value.Int(1), value.Int(2), value.Int(3), value.Int(3)),
+			nil},
+
+		{`
+arr := [];
+
+for i in 4 {
+	arr = append(arr,i);
+};
+
+len(arr);
+`, value.Int(4),
+			nil},
 	}
 
 	for _, test := range tests {
 		v, err := Exec(test.program, nil)
+
+		if test.expectedError != nil {
+			assert.EqualError(t, err, test.expectedError.Error())
+		} else {
+			assert.NoError(t, err)
+			assert.Equal(t, test.expectedValue, v)
+		}
+	}
+}
+
+func Test_builtinLen(t *testing.T) {
+	tests := []struct {
+		args          []value.Value
+		expectedValue value.Value
+		expectedError error
+	}{
+		{nil, nil, errors.New("len: требуется один аргумент")},
+
+		{[]value.Value{value.Array()}, value.Int(0), nil},
+		{[]value.Value{value.Array(value.Null())}, value.Int(1), nil},
+		{[]value.Value{value.Array(value.Null(), value.Null())}, value.Int(2), nil},
+
+		{[]value.Value{
+			value.Array(value.Null(), value.Null()),
+			value.Array(value.Null()),
+		}, value.Int(2), nil},
+	}
+
+	for _, test := range tests {
+		v, err := builtinLen(test.args...)
+
+		if test.expectedError != nil {
+			assert.EqualError(t, err, test.expectedError.Error())
+		} else {
+			assert.NoError(t, err)
+			assert.Equal(t, test.expectedValue, v)
+		}
+	}
+}
+
+func Test_builtinAppend(t *testing.T) {
+	tests := []struct {
+		args          []value.Value
+		expectedValue value.Value
+		expectedError error
+	}{
+		{nil, nil, errors.New("append: требуется один аргумент")},
+
+		{[]value.Value{value.Array()}, value.Array(), nil},
+		{[]value.Value{value.Array(), value.Int(512)}, value.Array(value.Int(512)), nil},
+		{
+			[]value.Value{value.Array(value.Int(512)), value.Int(2187)},
+			value.Array(value.Int(512), value.Int(2187)),
+			nil,
+		},
+		{
+			[]value.Value{value.Array(value.Int(512)), value.Int(2187), value.Int(1024)},
+			value.Array(value.Int(512), value.Int(2187), value.Int(1024)),
+			nil,
+		},
+	}
+
+	for _, test := range tests {
+		v, err := builtinAppend(test.args...)
 
 		if test.expectedError != nil {
 			assert.EqualError(t, err, test.expectedError.Error())
